73b852d1040d322db369fa6607605051ca72d238
Jeda777 <jakub.jeda@wp.pl>
2022-11-03 13:24:52 +0100

    Update Skill.svelte

--- src/components/Skill.svelte
+++ src/components/Skill.svelte
@@ -5,7 +5,6 @@
 <p class="skill" key={skill}>{skill}</p>
 
 <style lang="scss">
-  @import "../variables";
   .skill {
     @apply rounded-lg bg-primary py-1 px-3 font-normal text-bg transition-all duration-300 hover:opacity-70 md:rounded-[0.625rem] 
     md:py-1.5 md:px-3.5 lg:rounded-xl lg:py-2 lg:px-4 xl:text-lg 2xl:text-[22px];
