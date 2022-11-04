9d300ce97e9930bdf65e37a6056f964d30f17c0c
Jeda777 <jakub.jeda@wp.pl>
2022-11-04 14:51:07 +0100

    About style change

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -17,44 +17,29 @@
 <section id="about">
   <h2>About Me</h2>
   <div class="about-container">
-    <div class="about-text">
-      <h3>Me</h3>
-      <p>
-        My name is Jakub and I am a self-taught Front-end Developer from Poland.
-        I have been learning front-end for a year. I am currently in my first
-        year of studies in the field of front-end development.
-      </p>
-    </div>
-    <div class="about-skills">
-      <h3>Skills</h3>
-      <div class="skills-container">
-        {#each skills as skill}
-          <Skill {skill} />
-        {/each}
-      </div>
+    <p>
+      My name is Jakub and I am a self-taught Front-end Developer from Poland. I
+      have been learning front-end for a year. I am currently in my first year
+      of studies in the field of front-end development.
+    </p>
+    <div class="skills-container">
+      {#each skills as skill}
+        <Skill {skill} />
+      {/each}
     </div>
   </div>
 </section>
 
 <style lang="scss">
-  @import "../variables";
   #about {
     @apply px-12 md:px-32 lg:px-40 xl:px-8;
     .about-container {
       @apply flex flex-col items-center gap-8 md:gap-12 xl:flex-row xl:items-start xl:gap-40 xl:text-left 2xl:gap-48;
-      .about-text,
-      .about-skills {
-        @apply flex flex-col items-center gap-6 md:gap-8 xl:flex-1 xl:items-start;
-        h3 {
-          @apply text-[26px] font-semibold md:border-4 md:text-3xl xl:text-4xl;
-          border-bottom: 3px solid $text-hover;
-        }
-        p {
-          @apply font-normal xl:text-lg 2xl:text-2xl;
-        }
-        .skills-container {
-          @apply flex flex-wrap justify-center gap-2 md:gap-3 xl:justify-start;
-        }
+      p {
+        @apply font-normal md:text-lg xl:flex-1 xl:text-[20px] 2xl:text-2xl 3xl:text-[26px];
+      }
+      .skills-container {
+        @apply flex flex-wrap justify-center gap-2 md:gap-3 xl:flex-1 xl:justify-start;
       }
     }
   }
--- src/components/Skill.svelte
+++ src/components/Skill.svelte
@@ -6,7 +6,7 @@
 
 <style lang="scss">
   .skill {
-    @apply rounded-lg bg-primary py-1 px-3 font-normal text-bg transition-all duration-300 hover:opacity-70 md:rounded-[0.625rem] 
-    md:py-1.5 md:px-3.5 lg:rounded-xl lg:py-2 lg:px-4 xl:text-lg 2xl:text-[22px];
+    @apply rounded-lg bg-primary py-1 px-3 text-sm font-normal text-bg transition-all duration-300 hover:opacity-70 md:rounded-[0.625rem] md:py-1.5 
+    md:px-3.5 md:text-base lg:rounded-xl lg:py-2 lg:px-4 2xl:text-lg 3xl:text-[20px];
   }
 </style>
