017719cfd1d3adb5aef53bec1e157327396426ac
Jeda777 <jakub.jeda@wp.pl>
2022-11-17 22:00:58 +0100

    Projects update

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,47 +1,33 @@
 import smallProjectsImg from "./smallProjectsImg.png";
 import hangmanGameImg from "./hangmanGameImg.png";
+import foodielandImg from "./foodielandImg.png";
 
 const projects = [
   {
+    name: "Cards Against Humanity clone",
+    description:
+      "Fullstack game, React on frontend, Node.js and Express on backend and Socket.IO on both for two-way communication.",
+    imageURL:
+      "https://github.com/Jeda777/cards-against-humanity-clone/raw/main/screenshots/Screenshot1.png",
+    githubURL: "https://github.com/Jeda777/cards-against-humanity-clone",
+  },
+  {
+    name: "Foodieland cooking blog",
+    description:
+      "Cooking blog and recipe site made with Next.js, TypeScript and MongoDB.",
+    imageURL: foodielandImg,
+    githubURL: "https://github.com/Jeda777/foodieland-cooking-blog",
+    siteURL: "https://hilarious-puppy-107ba1.netlify.app/",
+  },
+  {
     name: "Rock, Paper, Scissors",
     description:
-      "Frontend Mentor Rock, Paper, Scissors challenge, PWA ready, using React, Typescript and SCSS",
+      "Frontend Mentor Rock, Paper, Scissors challenge, PWA ready, using React, TypeScript and SCSS",
     imageURL:
       "https://github.com/Jeda777/rock-paper-scissors-react-typescript-pwa-ready/raw/main/Screenshot2.png",
     githubURL:
       "https://github.com/Jeda777/rock-paper-scissors-react-typescript-pwa-ready",
     siteURL: "https://gentle-torte-e5192a.netlify.app/",
-  },
-  {
-    name: "HEX Color Game",
-    description:
-      "Game in which you need to choose which hex code describes showed color.",
-    imageURL:
-      "https://github.com/Jeda777/hex-color-game-typescript/raw/main/Screenshot.png",
-    githubURL: "https://github.com/Jeda777/hex-color-game-typescript",
-    siteURL: "https://jeda777.github.io/hex-color-game-typescript/",
-  },
-  // {
-  //   name: "Random Recipe",
-  //   description: "Site with random recipe from API.",
-  //   imageURL:
-  //     "https://github.com/Jeda777/random-recipe/raw/main/Screenshot.png",
-  //   githubURL: "https://github.com/Jeda777/random-recipe",
-  //   siteURL: "https://benevolent-tarsier-24cc2b.netlify.app/",
-  // },
-  {
-    name: "Hangman Game",
-    description: "Simple hangman game.",
-    imageURL: hangmanGameImg,
-    githubURL: "https://github.com/Jeda777/hangman-svelte",
-    siteURL: "https://celebrated-kulfi-471e87.netlify.app/",
-  },
-  {
-    name: "Small Projects",
-    description: "Site with collection of small HTML, CSS, and TS projects.",
-    imageURL: smallProjectsImg,
-    githubURL: "https://github.com/Jeda777/small-projects",
-    siteURL: "https://jeda777.github.io/small-projects/",
   },
   {
     name: "REST Coutries API With Theme Switcher",
@@ -54,14 +40,28 @@ const projects = [
     siteURL: "https://dreamy-alpaca-960c54.netlify.app/",
   },
   {
-    name: "UiDesignDaily Projects",
-    description:
-      "Site with a collection of projects based on uidesigndaily.com",
-    imageURL:
-      "https://github.com/Jeda777/uidesigndaily-clones/raw/main/Stats%20Cards-1213/Screenshot.png",
-    githubURL: "https://github.com/Jeda777/uidesigndaily-clones",
-    siteURL: "https://jeda777.github.io/uidesigndaily-clones/",
+    name: "Hangman Game",
+    description: "Simple hangman game made with SvelteKit and SCSS.",
+    imageURL: hangmanGameImg,
+    githubURL: "https://github.com/Jeda777/hangman-svelte",
+    siteURL: "https://celebrated-kulfi-471e87.netlify.app/",
   },
+  {
+    name: "HEX Color Game",
+    description:
+      "Game in which you need to choose which hex code describes showed color.",
+    imageURL:
+      "https://github.com/Jeda777/hex-color-game-typescript/raw/main/Screenshot.png",
+    githubURL: "https://github.com/Jeda777/hex-color-game-typescript",
+    siteURL: "https://jeda777.github.io/hex-color-game-typescript/",
+  },
+  // {
+  //   name: "Small Projects",
+  //   description: "Site with collection of small HTML, CSS, and TS projects.",
+  //   imageURL: smallProjectsImg,
+  //   githubURL: "https://github.com/Jeda777/small-projects",
+  //   siteURL: "https://jeda777.github.io/small-projects/",
+  // },
 ];
 
 export default projects;
--- src/components/ProjectLinks.svelte
+++ src/components/ProjectLinks.svelte
@@ -6,29 +6,36 @@
   export let type;
 </script>
 
-<a href={project.githubURL} class={type} target="_blank" rel="noreferrer">
+<a
+  href={project.githubURL}
+  class={`${type} ${project.siteURL === undefined ? "only" : ""}`}
+  target="_blank"
+  rel="noreferrer"
+>
   <img
     src={type === "overlay" ? github : githubDark}
     alt={`${project.name} github`}
   /></a
 >
-<a href={project.siteURL} class={type} target="_blank" rel="noreferrer">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke={type === "overlay" ? "#ffffff" : "#000000"}
-    class="h-6 w-6"
-    aria-label={`${project.name} website`}
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-    />
-  </svg>
-</a>
+{#if project.siteURL !== undefined}
+  <a href={project.siteURL} class={type} target="_blank" rel="noreferrer">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke={type === "overlay" ? "#ffffff" : "#000000"}
+      class="h-6 w-6"
+      aria-label={`${project.name} website`}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+      />
+    </svg>
+  </a>
+{/if}
 
 <style lang="scss">
   a {
@@ -41,6 +48,9 @@
   a.overlay {
     @apply absolute top-1/2 left-1/3;
     transform: translate(-50%, -50%);
+    &.only {
+      @apply left-1/2;
+    }
     &:nth-child(2) {
       @apply left-2/3;
     }
