2c495ac6b72b789b424ef7f8c4ef8704df11250e
Jeda777 <jakub.jeda@wp.pl>
2022-11-19 14:26:16 +0100

    Update projects.js

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -22,7 +22,7 @@ const projects = [
   {
     name: "Rock, Paper, Scissors",
     description:
-      "Frontend Mentor Rock, Paper, Scissors challenge, PWA ready, using React, TypeScript and SCSS",
+      "Frontend Mentor Rock, Paper, Scissors challenge, PWA ready, using React, TypeScript and SCSS.",
     imageURL:
       "https://github.com/Jeda777/rock-paper-scissors-react-typescript-pwa-ready/raw/main/Screenshot2.png",
     githubURL:
@@ -47,11 +47,11 @@ const projects = [
     siteURL: "https://celebrated-kulfi-471e87.netlify.app/",
   },
   {
-    name: "HEX Color Game",
+    name: "Color Game",
     description:
-      "Game in which you need to choose which hex code describes showed color.",
+      "In the game, you must select the correct color shown on the screen. Made with typescript.",
     imageURL:
-      "https://github.com/Jeda777/hex-color-game-typescript/raw/main/Screenshot.png",
+      "https://github.com/Jeda777/hex-color-game-typescript/raw/main/Screenshot1.png",
     githubURL: "https://github.com/Jeda777/hex-color-game-typescript",
     siteURL: "https://jeda777.github.io/hex-color-game-typescript/",
   },
